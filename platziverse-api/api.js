b0a9011ed9ea0f9968fb991752f5d015b531602b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-10-31 20:59:56 -0400

    Add integration with database module

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -2,8 +2,28 @@
 
 const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
+const db = require('platziverse-db')
+
+const config = require('./config')
 
 const api = express.Router()
+
+let services, Agent, Metric
+
+api.use('*', async (req, res, next) => {
+  if (!services) {
+    debug('Connecting to database')
+    try {
+      services = await db(config.db)
+    } catch (e) {
+      return next(e)
+    }
+
+    Agent = services.Agent
+    Metric = services.Metric
+  }
+  next()
+})
 
 api.get('/agents', (req, res) => {
   debug('A request has come to /agents')
