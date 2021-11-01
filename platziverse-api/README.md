7d51563309a25e786a4b4faf7d823cab84927a14
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-11-01 01:27:16 -0400

    Add integration tests with Ava, Supertest and Sinon

--- platziverse-api/README.md
+++ platziverse-api/README.md
@@ -5,11 +5,18 @@
 - Start server on dev mode: `npm run start-dev`
 - Run tests: `npm test`
 
+# Commands for test api with `http` command (similar to `curl`)
+- `http http://192.168.0.14:3000/api/agents`
+- `http http://192.168.0.14:3000/api/agent/yyyx`
+- `http http://192.168.0.14:3000/api/metrics/yyyx`
+- `http http://192.168.0.14:3000/api/metrics/yyyx/temp`
+
 ## Commands for install dependencies
 - `npm i standard nodemon --save-dev`
 - `npm install express debug --save`
 - `npm i chalk --save`
 - `npm install --save-dev ava supertest`
+- `npm install sinon proxyquire --save-dev`
 
 # Notes for package.json
 - Script `start-dev` for Windows: `set DEBUG=platziverse:* & nodemon server.js`
--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -2,8 +2,45 @@
 
 const test = require('ava')
 const request = require('supertest')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
-const server = require('../server')
+const agentFixtures = require('./fixtures/agent')
+
+let sandbox = null
+let server = null
+let dbStub = null
+const AgentStub = {}
+const MetricStub = {}
+const uuid = 'yyy-yyy-yyy'
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+
+  AgentStub.findConnected = sandbox.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  AgentStub.findByUuid = sandbox.stub()
+  AgentStub.findByUuid.withArgs(uuid).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
+  const api = proxyquire('../api', {
+    'platziverse-db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+})
+
+test.afterEach(() => {
+  sandbox && sinon.restore()
+})
 
 test.serial.cb('/api/agents', t => {
   request(server)
@@ -12,8 +49,31 @@ test.serial.cb('/api/agents', t => {
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.falsy(err, 'should not return an error')
-      const body = res.body
-      t.deepEqual(body, {}, 'response body should be the expected')
+      const body = JSON.stringify(res.body)
+      const expected = JSON.stringify(agentFixtures.connected)
+      t.deepEqual(body, expected, 'response body should be the expected')
       t.end()
     })
 })
+
+test.serial.cb('/api/agent/:uuid', t => {
+  request(server)
+    .get(`/api/agent/${uuid}`)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      t.falsy(err, 'should not return an error')
+      const body = JSON.stringify(res.body)
+      const expected = JSON.stringify(agentFixtures.byUuid(uuid))
+      t.deepEqual(body, expected, 'response body should be the expected')
+      t.end()
+    })
+})
+
+test.serial.todo('/api/agent/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid')
+test.serial.todo('/api/metrics/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid/:type')
+test.serial.todo('/api/metrics/:uuid/:type - not found')
